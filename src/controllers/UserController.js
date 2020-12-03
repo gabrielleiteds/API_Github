370600b8f7d7325f19525c5b21a4a66b9c247cf5
leitegs <leitedasilvag@gmail.com>
2020-12-02 23:13:32 -0300

    create functions for user

---
+++ src/controllers/UserController.js
@@ -0,0 +1,63 @@
+const User = require('../models/User');
+
+module.exports = {
+  async create(req, res) {
+    const { name, email, password, biography, avatar_url, username, latitude, longitude } = req.body;
+
+    const userExists = await User.findOne({
+      where: {
+        email
+      }
+    })
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      biography,
+      username,
+      avatar_url,
+      latitude,
+      longitude
+    });
+
+    user.password = undefined;
+
+    return res.status(201).json(user)
+  },
+  async show(req, res) {
+    const findUsers = await User.findAll()
+
+    return res.status(200).json(findUsers)
+  },
+  async update(req, res) {
+    const values = req.body;
+
+    const user_id = req.params;
+
+    const user = await User.findOne({
+      where: {
+        id: user_id
+      }
+    })
+
+    const update = await user.update(values)
+
+    return res.json(update)
+  },
+  async delete(req, res) {
+    const user_id = req.params;
+
+    const user = await User.destroy({
+      where: {
+        id: user_id
+      }
+    })
+
+    return res.json("Deleted")
+  }
+}
