9c10c27b9d7e1d040ce798532e682f5838fa1e1a
leitegs <leitedasilvag@gmail.com>
2020-12-02 23:26:08 -0300

    create routes from login and logout

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,16 @@ const routes = require('express').Router()
 
 //controllers
 const UserController = require('../controllers/UserController')
+const Authcontroller = require('../controllers/AuthController')
 
 //crud User
 routes.post('/users', UserController.create)
 routes.get('/users', UserController.show)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
+
+// Login and logout
+routes.post('/login', Authcontroller.authenticate);
+routes.post('/logout', Authcontroller.logoutUser);
 
 module.exports = routes;
