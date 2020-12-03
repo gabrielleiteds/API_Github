c36249b809e8584b561fa2b57d0f496ee96afd8f
leitegs <leitedasilvag@gmail.com>
2020-12-02 23:13:00 -0300

    created CRUD from users

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,12 @@
 const routes = require('express').Router()
 
+//controllers
+const UserController = require('../controllers/UserController')
 
+//crud User
+routes.post('/users', UserController.create)
+routes.get('/users', UserController.show)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
 
 module.exports = routes;
