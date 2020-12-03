c9e87996571803b762f839278f16cc5bdc6997be
leitegs <leitedasilvag@gmail.com>
2020-12-03 00:12:36 -0300

    create routes for creation of repositories

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,10 @@ const routes = require('express').Router()
 //controllers
 const UserController = require('../controllers/UserController')
 const Authcontroller = require('../controllers/AuthController')
+const RepositoryController = require('../controllers/RepositoryController')
+
+//middleware
+const authentication = require('../middleware/auth')
 
 //crud User
 routes.post('/users', UserController.create)
@@ -13,5 +17,10 @@ routes.delete('/users/:id', UserController.delete)
 // Login and logout
 routes.post('/login', Authcontroller.authenticate);
 routes.post('/logout', Authcontroller.logoutUser);
+
+routes.use(authentication)
+
+routes.get('/profile', RepositoryController.show)
+routes.post('/repositories', RepositoryController.create)
 
 module.exports = routes;
