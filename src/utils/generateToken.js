ec264a6af921153b30987191ad152176e38fba48
leitegs <leitedasilvag@gmail.com>
2020-12-02 23:26:49 -0300

    create function for generate token JWT

---
+++ src/utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(params = {}) {
+  return jwt.sign(params, process.env.SECRET, {
+    expiresIn: '1d'
+  });
+}
+
+module.exports = generateToken;
